5f73cae2aa736ecda96efee80febdbd117039fe8
Afsal102 <39826977+Afsal102@users.noreply.github.com>
2021-08-02 20:35:33 +0530

    initial commit

---
+++ clients/directory_client/android/app/src/main/kotlin/com/afzal/directory_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.afzal.directory_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ clients/directory_testing/android/app/src/main/kotlin/com/afzal/directory_testing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.afzal.directory_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
